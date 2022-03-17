7da1475ac8a3614b62b1398a1daa16291567025f
Naveen_Kumar_Singh <naveen_kumar_singh@epam.com>
2022-03-17 17:30:21 +0530

    Initial commit

---
+++ src/main/java/neo/springexpressionlanguage/SpringExpressionLanguageApplication.java
@@ -0,0 +1,42 @@
+package neo.springexpressionlanguage;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@SpringBootApplication
+
+public class SpringExpressionLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExpressionLanguageApplication.class, args);
+	}
+
+}
+
+@Component("uuid")
+class UuidService {
+	public String buildUuid() {
+		return UUID.randomUUID().toString();
+	}
+}
+@Component
+class Bar{
+	private final Foo foo;
+	private final Log log = LogFactory.getLog(getClass());
+	Bar(Foo foo,@Value("#{uuid.buildUuid()}") String uuid,
+			@Value("#{ 2>1}") boolean fact){
+		this.foo=foo;
+		this.log.info("uuid :"+uuid);
+		log.info("fact :"+fact);
+	}
+}
+@Component()
+class Foo{
+}
---
+++ src/test/java/neo/springexpressionlanguage/SpringExpressionLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package neo.springexpressionlanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExpressionLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
